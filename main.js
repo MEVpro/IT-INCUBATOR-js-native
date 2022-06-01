646cf7194152249e1328c76ea009a75b6642fec6
Evgeny Mitin <101546611+MEVpro@users.noreply.github.com>
2022-06-01 21:49:05 +0300

    Lesson 2

--- main.js
+++ main.js
@@ -1,82 +1,127 @@
-/*
-console.log("yahoo")
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    }
+];
+// map:
+// const names = ["Bob", "Alex", "Nick", "John"]
 
-const user = {  // ссылка на созданный объект
-    name: "Bob",
-    age: 26,
-    isMarried: true
+const getNames = (arr) => {
+    const result = []
+////////////////
+    const func = (st) => st.name
+////////////////
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
 }
-const arr = [1,2,3,4,5]
 
-const copyUser = user // тегу copyUser присвоена ссылка объекта user
-                      // copyUser не объект тк нет {}
-                      // new Object(...) - создание объекта {}
-                      // new Array(...) - создание массива []
-                      // Object.assign({}, ...) - создание нового объекта {} с присвоение ему парраметров оъекта ...
+console.log(getNames(students))
 
-user.name = 'Anna'
-console.log(user.name)
+const addScores = (arr) => {
+    const result = []
+////////////////
+    const func = (st) => ({...st, scores: st.scores + 10})
+////////////////
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
 
-const realCopyUser = {...user} // новый объект realCopyUser в котором нежат данные user
-// realCopyUser.name = user.name
-// realCopyUser.age = user.age
-// realCopyUser.isMarried = user.isMarried
-console.log(realCopyUser)
-console.log(realCopyUser === user) // false тк это разные объекты с разными на них ссылками
-// "..." - spread operator
 
-const realCopyArr = [...arr] // новый массив realCopyArr с данными массива arr
+console.log(addScores(students))
 
-*/
+//  {
+//         name: "Bob",
+//         age: 22,
+//         isMarried: true,
+//         scores: 85,
+//         isStudent: true
+//  }
 
-const user = {  // ссылка на созданный объект
-    name: "Bob",
-    age: 26,
-    isMarried: true,
-    friends: ["Alex", "Bob", "John"],
-    address: {
-        city: "Bobruisk",
-        street: "Lenina"
+const addPropIsStudent = (arr) => {
+    const result = []
+////////////////
+    const func = (st) => ({...st, isStudent: true})
+////////////////
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+console.log(addPropIsStudent(students))
+
+
+const getMappedArray = (arr, func) => {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])
+        result.push(newValue)
+    }
+    return result
+}
+
+console.log(getMappedArray(students, (st) => st.name))
+console.log(getMappedArray(students, (st) => ({...st, isStudent: true})))
+console.log(students.map((st) => st.name))
+
+
+// filter:
+console.log(students.filter(st => st.scores >= 100))
+
+const myFilter = (arr, func) => {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])  // => true или false
+        if (newValue === true){
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+const callbackFn = st => st.scores >= 100
+console.log(myFilter(students, callbackFn))
+// find:
+const myFind = (arr, func) => {
+    for (let i = 0; i < arr.length; i++) {
+        const newValue = func(arr[i])  // => true или false
+        if (newValue === true){
+            return arr[i]
+        }
     }
 }
 
-const copyUser = {...user}
-copyUser.friends.push("Anna")
-console.log(user)
 
-// Поверхностная копия (shallow copy) - это копия без учета вложенности других обеътов
-// Глубокая копия (deep copy) - это копия с учетом вложенности других объектов
-
-const deepCopy = {...user, friends: [...user.friends]}
-
-// создаем новый объект "deepCopy" и присваеваем ему данные объекта "user"
-// но создадим(пересоздадим) новый массив "friends: [...user.friends]" внути объектс "deepCopy"
-// и присвоем ему параметры массива friends
-
-const deepCopyUser = {...user, friends: [...user.friends], address: {...user.address}}
-
-// structuredClone() - автоматически делает глубокую копию объекта
-// const deepCopyUser = structuredClone(user)
-
-const students = [
-    {name: "Bob"},
-    {name: "Alex"},
-    {name: "Ann"},
-]
-
-const shallowCopyStudents = {...students} // копируем массив спредом тк объекты внутри него простые
-
-const deepCopyStudents = []
-for (let i = 0; i < students.length; i++) {
-    deepCopyStudents[i] = {...students[i]}
-}
-
-const deepCopyMapFunctionStudents = students.map ( student => ({...student}))
-console.log(deepCopyMapFunctionStudents)
-
-
-
-
+console.log(students.find(st => st.name === "Alex"))
+console.log(myFind(students, st => st.name === "Alex"))
 
 
 
